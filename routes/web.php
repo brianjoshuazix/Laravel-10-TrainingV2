4ff7ee846366456f0c371d17b22231db69c40554
Brian Joshua Laptop <brianjoshuazix@gmail.com>
2024-09-10 01:01:35 +0800

    Episode 9 done

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+use function Laravel\Prompts\select;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +18,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // fetch all users
+    // $users = DB::select("select * from users");
+    $users = DB::table('users')->find(1);
+
+    // create new users
+    // $user = DB::insert('insert into users (name, email, password) values (?, ?, ?)', [
+    //     'Azzahra',
+    //     'az@gmail.com',
+    //     '12345']);
+    // $user = DB::table('users')->insert([
+    //     'name' => 'Zahra',
+    //     'email' => 'zahra@gmail.com',
+    //     'password' => 'zahra'
+    // ]);
+
+    // update users
+    // $user = DB::update("update users set email=? where id=?", [
+    //     'az123@gmail.com',
+    //     2,
+    // ]);
+    // $user = DB::table('users')->where('id', 3)->update(['email' => 'zahra145@gmail.com']);
+
+    // delete a user
+    // $user = DB::delete("delete from users where id=2");
+    // $user = DB::table('users')->where('id', 3)->delete();
+
+    dd($users);
 });
 
 Route::get('/dashboard', function () {
@@ -28,4 +57,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
