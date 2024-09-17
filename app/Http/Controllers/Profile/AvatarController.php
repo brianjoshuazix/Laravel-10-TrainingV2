ef26893b26b20863bb57df562097ebd38cbbad06
Brian Joshua Laptop <brianjoshuazix@gmail.com>
2024-09-18 00:59:26 +0800

    Episode 21 Done

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -5,19 +5,28 @@ namespace App\Http\Controllers\Profile;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateAvatarRequest;
 use Illuminate\Http\Request;
+use Storage;
 
 class AvatarController extends Controller
 {
-    public function update(UpdateAvatarRequest $request) {
+    public function update(UpdateAvatarRequest $request)
+    {
         // store avatar field
         // return back()->with('message', 'Avatar is Changed');
 
+        $path = Storage::disk('public')->put('avatars',$request->file('avatar'));
+
+        // dd($path);
 
 
-        $path = $request->file('avatar')->store('avatars', 'public');
+        // $path = $request->file('avatar')->store('avatars', 'public');
+
+
+        if ($oldAvatar = $request->user()->avatar) {
+            Storage::disk('public')->delete($oldAvatar);
+        }
+
         auth()->user()->update(['avatar' => $path]);
-
-
 
         return redirect(route('profile.edit'))->with('message', 'Avatar is updated');
     }
